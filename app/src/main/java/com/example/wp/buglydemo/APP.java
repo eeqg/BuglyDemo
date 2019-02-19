ec2cf5820e79172560b0a44d55239740261bafb6
eeqg <876583632@qq.com>
2019-02-19 15:27:12 +0800

    +bugly.

--- app/src/main/java/com/example/wp/buglydemo/APP.java
+++ app/src/main/java/com/example/wp/buglydemo/APP.java
@@ -2,14 +2,13 @@ package com.example.wp.buglydemo;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Environment;
 import android.support.multidex.MultiDex;
 import android.widget.Toast;
 
-import com.example.wp.buglydemo.utils.LogUtils;
 import com.tencent.bugly.Bugly;
 import com.tencent.bugly.beta.Beta;
 import com.tencent.bugly.beta.interfaces.BetaPatchListener;
-import com.tencent.bugly.beta.upgrade.UpgradeStateListener;
 
 import java.util.Locale;
 
@@ -22,7 +21,8 @@ public class APP extends Application {
 	public void onCreate() {
 		super.onCreate();
 		
-		initBugly();
+		// initBuglyUpgrade();
+		configTinker();
 	}
 	
 	@Override
@@ -37,98 +37,246 @@ public class APP extends Application {
 	}
 	
 	/**
+	 * version upgrade.
+	 */
+	private void initBuglyUpgrade() {
+		/**
+		 * true表示app启动自动初始化升级模块；
+		 * false不好自动初始化
+		 * 开发者如果担心sdk初始化影响app启动速度，可以设置为false
+		 * 在后面某个时刻手动调用
+		 */
+		Beta.autoInit = true;
+		
+		/**
+		 * true表示初始化时自动检查升级
+		 * false表示不会自动检查升级，需要手动调用Beta.checkUpgrade()方法
+		 */
+		Beta.autoCheckUpgrade = true;
+		
+		/**
+		 * 设置启动延时为1s（默认延时3s），APP启动1s后初始化SDK，避免影响APP启动速度;
+		 */
+		Beta.initDelay = 1 * 1000;
+		
+		/**
+		 * 设置通知栏大图标，largeIconId为项目中的图片资源；
+		 */
+		Beta.largeIconId = R.mipmap.ic_launcher;
+		
+		/**
+		 * 设置状态栏小图标，smallIconId为项目中的图片资源id;
+		 */
+		Beta.smallIconId = R.mipmap.ic_launcher;
+		
+		
+		/**
+		 * 设置更新弹窗默认展示的banner，defaultBannerId为项目中的图片资源Id;
+		 * 当后台配置的banner拉取失败时显示此banner，默认不设置则展示“loading“;
+		 */
+		Beta.defaultBannerId = R.mipmap.ic_launcher;
+		
+		/**
+		 * 设置sd卡的Download为更新资源保存目录;
+		 * 后续更新资源会保存在此目录，需要在manifest中添加WRITE_EXTERNAL_STORAGE权限;
+		 */
+		Beta.storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+		
+		/**
+		 * 点击过确认的弹窗在APP下次启动自动检查更新时会再次显示;
+		 */
+		Beta.showInterruptedStrategy = true;
+		
+		/**
+		 * 只允许在MainActivity上显示更新弹窗，其他activity上不显示弹窗;
+		 * 不设置会默认所有activity都可以显示弹窗;
+		 */
+		Beta.canShowUpgradeActs.add(MainActivity.class);
+		
+		
+		/**
+		 *  设置自定义升级对话框UI布局
+		 *  注意：因为要保持接口统一，需要用户在指定控件按照以下方式设置tag，否则会影响您的正常使用：
+		 *  标题：beta_title，如：android:tag="beta_title"
+		 *  升级信息：beta_upgrade_info  如： android:tag="beta_upgrade_info"
+		 *  更新属性：beta_upgrade_feature 如： android:tag="beta_upgrade_feature"
+		 *  取消按钮：beta_cancel_button 如：android:tag="beta_cancel_button"
+		 *  确定按钮：beta_confirm_button 如：android:tag="beta_confirm_button"
+		 *  详见layout/upgrade_dialog.xml
+		 */
+		//        Beta.upgradeDialogLayoutId = R.layout.upgrade_dialog;
+		
+		/**
+		 * 设置自定义tip弹窗UI布局
+		 * 注意：因为要保持接口统一，需要用户在指定控件按照以下方式设置tag，否则会影响您的正常使用：
+		 *  标题：beta_title，如：android:tag="beta_title"
+		 *  提示信息：beta_tip_message 如： android:tag="beta_tip_message"
+		 *  取消按钮：beta_cancel_button 如：android:tag="beta_cancel_button"
+		 *  确定按钮：beta_confirm_button 如：android:tag="beta_confirm_button"
+		 *  详见layout/tips_dialog.xml
+		 */
+		//        Beta.tipsDialogLayoutId = R.layout.tips_dialog;
+		
+		/**
+		 *  如果想监听升级对话框的生命周期事件，可以通过设置OnUILifecycleListener接口
+		 *  回调参数解释：
+		 *  context - 当前弹窗上下文对象
+		 *  view - 升级对话框的根布局视图，可通过这个对象查找指定view控件
+		 *  upgradeInfo - 升级信息
+		 */
+     /*  Beta.upgradeDialogLifecycleListener = new UILifecycleListener<UpgradeInfo>() {
+            @Override
+            public void onCreate(Context context, View view, UpgradeInfo upgradeInfo) {
+                Log.d(TAG, "onCreate");
+                // 注：可通过这个回调方式获取布局的控件，如果设置了id，可通过findViewById方式获取，如果设置了tag，可以通过findViewWithTag，具体参考下面例子:
+
+                // 通过id方式获取控件，并更改imageview图片
+                ImageView imageView = (ImageView) view.findViewById(R.id.imageview);
+                imageView.setImageResource(R.mipmap.ic_launcher);
+
+                // 通过tag方式获取控件，并更改布局内容
+                TextView textView = (TextView) view.findViewWithTag("textview");
+                textView.setText("my custom text");
+
+                // 更多的操作：比如设置控件的点击事件
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), OtherActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @Override
+            public void onStart(Context context, View view, UpgradeInfo upgradeInfo) {
+                Log.d(TAG, "onStart");
+            }
+
+            @Override
+            public void onResume(Context context, View view, UpgradeInfo upgradeInfo) {
+                Log.d(TAG, "onResume");
+            }
+
+            @Override
+            public void onPause(Context context, View view, UpgradeInfo upgradeInfo) {
+                Log.d(TAG, "onPause");
+            }
+
+            @Override
+            public void onStop(Context context, View view, UpgradeInfo upgradeInfo) {
+                Log.d(TAG, "onStop");
+            }
+
+            @Override
+            public void onDestroy(Context context, View view, UpgradeInfo upgradeInfo) {
+                Log.d(TAG, "onDestory");
+            }
+        };*/
+		
+		/**
+		 * 自定义Activity参考，通过回调接口来跳转到你自定义的Actiivty中。
+		 */
+       /* Beta.upgradeListener = new UpgradeListener() {
+
+            @Override
+            public void onUpgrade(int ret, UpgradeInfo strategy, boolean isManual, boolean isSilence) {
+                if (strategy != null) {
+                    Intent i = new Intent();
+                    i.setClass(getApplicationContext(), UpgradeActivity.class);
+                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(getApplicationContext(), "没有更新", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };*/
+		
+		
+		/**
+		 * 已经接入Bugly用户改用上面的初始化方法,不影响原有的crash上报功能;
+		 * init方法会自动检测更新，不需要再手动调用Beta.checkUpdate(),如需增加自动检查时机可以使用Beta.checkUpdate(false,false);
+		 * 参数1： applicationContext
+		 * 参数2：appId
+		 * 参数3：是否开启debug
+		 */
+		// Bugly.init(getApplicationContext(), Const.BUGLY_APP_ID, true); //@{initBuglyHotfix()}
+		
+		/**
+		 * 如果想自定义策略，按照如下方式设置
+		 */
+		
+		/***** Bugly高级设置 *****/
+		//        BuglyStrategy strategy = new BuglyStrategy();
+		/**
+		 * 设置app渠道号
+		 */
+		//        strategy.setAppChannel(APP_CHANNEL);
+		
+		//        Bugly.init(getApplicationContext(), APP_ID, true, strategy);
+	}
+	
+	/**
 	 * init tinker hot-fix.
 	 */
-	private void initBugly() {
+	private void configTinker() {
 		// 设置是否开启热更新能力，默认为true
 		Beta.enableHotfix = true;
-		// 设置是否自动下载补丁
+		// 设置是否自动下载补丁，默认为true
 		Beta.canAutoDownloadPatch = true;
-		// 设置是否提示用户重启
-		Beta.canNotifyUserRestart = true;
-		// 设置是否自动合成补丁
+		// 设置是否自动合成补丁，默认为true
 		Beta.canAutoPatch = true;
-		
-		//全量升级状态回调
-		Beta.upgradeStateListener = new UpgradeStateListener() {
-			@Override
-			public void onUpgradeFailed(boolean b) {
-			
-			}
-			
-			@Override
-			public void onUpgradeSuccess(boolean b) {
-			
-			}
-			
-			@Override
-			public void onUpgradeNoVersion(boolean b) {
-				Toast.makeText(getApplicationContext(), "最新版本", Toast.LENGTH_SHORT).show();
-			}
-			
-			@Override
-			public void onUpgrading(boolean b) {
-				Toast.makeText(getApplicationContext(), "onUpgrading", Toast.LENGTH_SHORT).show();
-			}
-			
-			@Override
-			public void onDownloadCompleted(boolean b) {
-			
-			}
-		};
-		
-		//补丁回调接口，可以监听补丁接收、下载、合成的回调
+		// 设置是否提示用户重启，默认为false
+		Beta.canNotifyUserRestart = true;
+		// 补丁回调接口
 		Beta.betaPatchListener = new BetaPatchListener() {
 			@Override
-			public void onPatchReceived(String patchFileUrl) {
-				Toast.makeText(getApplicationContext(), patchFileUrl, Toast.LENGTH_SHORT).show();
+			public void onPatchReceived(String patchFile) {
+				Toast.makeText(getApplicationContext(), "补丁下载地址" + patchFile, Toast.LENGTH_SHORT).show();
 			}
 			
 			@Override
 			public void onDownloadReceived(long savedLength, long totalLength) {
-				Toast.makeText(getApplicationContext(), String.format(Locale.getDefault(),
-						"%s %d%%",
-						Beta.strNotificationDownloading,
-						(int) (totalLength == 0 ? 0 : savedLength * 100 / totalLength)), Toast.LENGTH_SHORT).show();
+				Toast.makeText(getApplicationContext(),
+						String.format(Locale.getDefault(), "%s %d%%",
+								Beta.strNotificationDownloading,
+								(int) (totalLength == 0 ? 0 : savedLength * 100 / totalLength)),
+						Toast.LENGTH_SHORT).show();
 			}
 			
 			@Override
-			public void onDownloadSuccess(String patchFilePath) {
-				Toast.makeText(getApplicationContext(), patchFilePath, Toast.LENGTH_SHORT).show();
-				//                Beta.applyDownloadedPatch();
+			public void onDownloadSuccess(String msg) {
+				Toast.makeText(getApplicationContext(), "补丁下载成功", Toast.LENGTH_SHORT).show();
 			}
 			
 			@Override
 			public void onDownloadFailure(String msg) {
-				Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+				Toast.makeText(getApplicationContext(), "补丁下载失败", Toast.LENGTH_SHORT).show();
 			}
 			
 			@Override
 			public void onApplySuccess(String msg) {
-				Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+				Toast.makeText(getApplicationContext(), "补丁应用成功", Toast.LENGTH_SHORT).show();
 			}
 			
 			@Override
 			public void onApplyFailure(String msg) {
-				Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+				Toast.makeText(getApplicationContext(), "补丁应用失败", Toast.LENGTH_SHORT).show();
 			}
 			
 			@Override
 			public void onPatchRollback() {
-				Toast.makeText(getApplicationContext(), "onPatchRollback", Toast.LENGTH_SHORT).show();
+			
 			}
 		};
 		
-		long start = System.currentTimeMillis();
-		Bugly.setUserId(this, "falue");
-		Bugly.setUserTag(this, 123456);
-		Bugly.putUserData(this, "key1", "123");
-		Bugly.setAppChannel(this, "bugly");
-		
-		
-		// 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId,调试时将第三个参数设置为true
-		Bugly.init(this, "3f1b1bb5e9", true);
-		long end = System.currentTimeMillis();
-		LogUtils.d("init time--->" + (end - start + "ms"));
+		// 设置开发设备，默认为false，上传补丁如果下发范围指定为“开发设备”，需要调用此接口来标识开发设备
+		// Bugly.setIsDevelopmentDevice(getApplicationContext(), false);
+		// 多渠道需求塞入
+		// String channel = WalleChannelReader.getChannel(getApplication());
+		// Bugly.setAppChannel(getApplication(), channel);
+		// 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId
+		Bugly.init(getApplicationContext(), Const.BUGLY_APP_ID, true);
 	}
 }
--- app/src/main/java/com/example/wp/buglydemo/MainActivity.java
+++ app/src/main/java/com/example/wp/buglydemo/MainActivity.java
@@ -2,14 +2,59 @@ package com.example.wp.buglydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.tencent.bugly.beta.Beta;
+import com.tencent.bugly.beta.UpgradeInfo;
 
 public class MainActivity extends AppCompatActivity {
-
+	
+	TextView tvContent;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		TextView tvVersionInfo = findViewById(R.id.tvVersionInfo);
+		tvVersionInfo.setText(String.format("v%s %s", BuildConfig.VERSION_NAME, BuildConfig.BUILD_TYPE));
 		
+		tvContent = findViewById(R.id.tvContent);
+		
+		Beta.checkUpgrade();
+		
+		loadUpgradeInfo();
+	}
+	
+	private void loadUpgradeInfo() {
+		if (tvContent == null)
+			return;
+		
+		/***** 获取升级信息 *****/
+		UpgradeInfo upgradeInfo = Beta.getUpgradeInfo();
+		
+		if (upgradeInfo == null) {
+			tvContent.setText("无升级信息");
+			return;
+		}
+		StringBuilder info = new StringBuilder();
+		info.append("id: ").append(upgradeInfo.id).append("\n");
+		info.append("标题: ").append(upgradeInfo.title).append("\n");
+		info.append("升级说明: ").append(upgradeInfo.newFeature).append("\n");
+		info.append("versionCode: ").append(upgradeInfo.versionCode).append("\n");
+		info.append("versionName: ").append(upgradeInfo.versionName).append("\n");
+		info.append("发布时间: ").append(upgradeInfo.publishTime).append("\n");
+		info.append("安装包Md5: ").append(upgradeInfo.apkMd5).append("\n");
+		info.append("安装包下载地址: ").append(upgradeInfo.apkUrl).append("\n");
+		info.append("安装包大小: ").append(upgradeInfo.fileSize).append("\n");
+		info.append("弹窗间隔（ms）: ").append(upgradeInfo.popInterval).append("\n");
+		info.append("弹窗次数: ").append(upgradeInfo.popTimes).append("\n");
+		info.append("发布类型（0:测试 1:正式）: ").append(upgradeInfo.publishType).append("\n");
+		info.append("弹窗类型（1:建议 2:强制 3:手工）: ").append(upgradeInfo.upgradeType).append("\n");
+		info.append("图片地址：").append(upgradeInfo.imageUrl);
+		
+		
+		tvContent.setText(info);
 	}
 }
